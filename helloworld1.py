314516329c26696a64d86cd12a04f5be8c24882a
marvsticks <marvsticks123@gmail.com>
2024-06-24 15:16:48 +0100

    Update helloworld1.py
    adding variables

--- helloworld1.py
+++ helloworld1.py
@@ -3,3 +3,24 @@ print("Sarah")
 print("Kenny")
 print("Timi")
 print("oba")
+
+
+length = 5
+breath = 4
+length = 90
+
+area = length + breath
+print(area)
+
+
+
+a = 6
+b = 10
+h = 8
+area = 1/2*(a+b)*h
+print(area)
+
+b = 5
+h = 90
+area = 1/2 * (b*h)
+print(area)
