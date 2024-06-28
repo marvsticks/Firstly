dd777bc4b5f34b4f003301215dd9323a94a29b18
marvsticks <marvsticks123@gmail.com>
2024-06-28 10:28:05 +0100

    Update Marvellous.py

--- Marvellous.py
+++ Marvellous.py
@@ -1,1 +1,16 @@
 print("I love food and games")
+
+
+
+name = input("Write your name")
+age = (input("Enter your age"))
+hubby = input("Enter your hubby")
+Profile = ("Hi "  +  name +  " you are "  +  age  +  " years old and your hobby is "  +  hubby )
+print(Profile)
+
+
+given_name = input("Write your name")
+first_letter = (given_name[0])
+last_letter = (given_name[-1])
+print("The first letter of your beautiful name is " + first_letter + " and the last letter is " + last_letter)
+print(first_letter + last_letter)
